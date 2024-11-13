9f5c3c8e2d1b3bd76a09ca1392013de91de131bb
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-11-13 14:20:12 +0100

    feat: Removed ignored cache directory

--- download_web.php
+++ download_web.php
@@ -7,7 +7,7 @@ ini_set('max_execution_time', '3600');
 ini_set('set_time_limit', '0');
 set_time_limit(3600);
 
-$exclude = array('_test', 'cache');
+$exclude = array('_test');
 
 function addFileToTar($tar, $filePath, $archivePath) {
 	global $exclude;
